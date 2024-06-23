511c3e629f9503d78bb8d1cb40f19b7cf3486d2f
lionel-prats <lionel.prats.c@gmail.com>
2024-06-23 18:56:49 -0300

    Sección 43: FreshCoffee - Primeros Pasos con el Proyecto de React - Tailwind, React Router

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,11 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
+import { RouterProvider } from 'react-router-dom' // v258
+import router from './router' // importacion de src\router.jsx (v258)
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>,
 )
